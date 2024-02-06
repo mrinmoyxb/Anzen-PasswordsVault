be17868e6a37253bd654169f598c81d58b016fe1
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-06 23:53:03 +0530

    built the structure of the button need modifications

---
+++ app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/CustomCheckBox.kt
@@ -0,0 +1,78 @@
+package com.example.passwordmanager.View.PassWordGenerator.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.passwordmanager.R
+
+@Composable
+fun CustomCheckBox(label: String){
+    var checked by remember{mutableStateOf(false)}
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+    Box(
+        modifier = Modifier
+            .height(40.dp)
+            .width(40.dp)
+            .clickable { checked = !checked}
+            .background(if (checked) colorResource(id = R.color.thumb1) else Color.Transparent)
+            .clip(RoundedCornerShape(40.dp)),
+        contentAlignment = Alignment.Center
+    ){
+        if(checked){
+            Icon(imageVector = Icons.Default.Check,
+                contentDescription = "",
+                tint = Color.White)
+        }
+            
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            label, fontSize = 20.sp, color = Color.White
+        )
+    }
+
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayPasswords() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_color))
+    ) {
+        Spacer(modifier = Modifier.height(18.dp))
+        CustomCheckBox(label = "uppercase")
+    }
+}
