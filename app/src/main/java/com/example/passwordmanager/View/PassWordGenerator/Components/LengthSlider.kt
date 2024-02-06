f11c9cb47bc7d7d3e2036b4f36fbb969543c89a1
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-06 20:16:34 +0530

    fixed the slider, need to work with color and UI

--- app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/LengthSlider.kt
+++ app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/LengthSlider.kt
@@ -6,7 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,32 +21,74 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.passwordmanager.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LengthSlider(){
     var sliderValue by remember{
-        mutableStateOf(8.0f)
+        mutableStateOf(8)
     }
     Box(
-        modifier = Modifier.fillMaxWidth().padding(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
         contentAlignment = Alignment.Center
     ) {
         Slider(
-            value = sliderValue, onValueChange = { newValue ->
-                sliderValue = newValue
+            value = sliderValue.toFloat(), // Convert for Slider
+            onValueChange = { newSliderValue ->
+                sliderValue = newSliderValue.toInt() // Update state with integer
             },
             valueRange = 8f..20f,
-            steps = 0
+            steps = 0,
+            thumb = {
+                    Box(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(30.dp)
+                            .background(
+                                colorResource(
+                                    id = R.color.thumb1
+                                )
+                            ),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .size(30.dp)
+                                .padding(5.dp)
+                                .background(
+                                    colorResource(
+                                        id = R.color.white
+                                    ),
+                                    shape = RoundedCornerShape(30.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(sliderValue.toString(), textAlign = TextAlign.Center)
+                        }
+
+                    }
+            },
+            colors = SliderDefaults.colors(
+                thumbColor = colorResource(id = R.color.thumb1),
+                activeTrackColor = colorResource(id = R.color.thumb1),
+                inactiveTrackColor = Color.White
+            )
         )
     }
     Text("slider value: ${sliderValue}", color = Color.White, fontSize = 32.sp)
 }
+
 
 @Preview(showBackground = true)
 @Composable
