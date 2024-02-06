3e82ccb0d139eb552b27d322ef100333bb730d71
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-06 18:03:51 +0530

    Add files via upload

---
+++ random_password.py
@@ -0,0 +1,21 @@
+import random
+
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = lowercase.upper()
+symbols = "~`! @#$%^&*()_-+={[}]|\:;<,>.?/"
+numbers = "1234567890"
+
+
+all = lowercase + uppercase + symbols + numbers
+
+length = 16
+
+password = ""
+
+
+for p in range(length):
+    password = "".join([password, random.choice(all)])
+
+
+print(password)
+
