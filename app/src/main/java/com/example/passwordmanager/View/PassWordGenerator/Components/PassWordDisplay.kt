75af8ab3d9b98db121f1670d5485996c4fb43477
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-07 23:33:46 +0530

    completed the ui

--- app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/PassWordDisplay.kt
+++ app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/PassWordDisplay.kt
@@ -1,14 +1,17 @@
 package com.example.passwordmanager.View.PassWordGenerator.Components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -20,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,12 +43,24 @@ fun PassWordDisplay(password: String){
         colors = CardDefaults.cardColors(Color.Transparent)
     ){
         Box(
-            modifier = Modifier.fillMaxSize().background(Color.White),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
                 //.background(Brush.linearGradient(c1)),
             contentAlignment = Alignment.Center,
-        ){
-            Text(password, fontSize = 30.sp, color = Color.Black,
-                fontFamily = roboto, fontWeight = FontWeight.Bold)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(5.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(painter = painterResource(id = R.drawable.lock_img), contentDescription = "", modifier = Modifier.size(30.dp))
+                Spacer(modifier = Modifier.width(15.dp))
+                Text(
+                    password, fontSize = 30.sp, color = Color.Black,
+                    fontFamily = roboto, fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
