5ad8c4fc278e08a07bda6565c83757893a10fec6
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-07 13:33:35 +0530

    completed the refresh button

--- app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/RefreshButton.kt
+++ app/src/main/java/com/example/passwordmanager/View/PassWordGenerator/Components/RefreshButton.kt
@@ -1,92 +1,58 @@
 package com.example.passwordmanager.View.PassWordGenerator.Components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.passwordmanager.R
-import com.example.passwordmanager.ui.theme.roboto
 
 @Composable
-fun CopyToClipboard(){
+fun RefreshButton(){
     Card(
         modifier = Modifier
             .height(100.dp)
-            .width(250.dp),
+            .width(100.dp),
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(Color.Transparent)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(
-                    colorResource(id = R.color.thumb1),
-                    shape = RoundedCornerShape(20.dp)
-                ),
+                .background(colorResource(id = R.color.thumb1), shape = RoundedCornerShape(20.dp)),
             contentAlignment = Alignment.Center
-
         ) {
-            Row(
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.copy_icon),
-                    contentDescription = "copy",
-                    modifier = Modifier
-                        .size(30.dp)
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                    "Copy to clipboard",
-                    fontSize = 22.sp,
-                    color = Color.Black,
-                    fontFamily = roboto,
-                    fontWeight = FontWeight.Medium
-                )
-            }
+            Icon(painter = painterResource(id = R.drawable.refresh),
+                contentDescription = "refresh",
+                modifier = Modifier.size(50.dp))
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DisplayCopyToClipboards() {
+fun DisplayRefreshButton(){
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.background_color))
             .padding(10.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            CopyToClipboard()
-            RefreshButton()
+        RefreshButton()
     }
-}
 }
