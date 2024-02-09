cbb5f15395e2226f14a61cacca6240516d724ce2
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-09 20:46:51 +0530

    Input field

---
+++ app/src/main/java/com/example/passwordmanager/View/NewPassword/Componenets/InputCard.kt
@@ -0,0 +1,93 @@
+package com.example.passwordmanager.View.NewPassword.Componenets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.passwordmanager.R
+import com.example.passwordmanager.ui.theme.roboto
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputCard(heading: String, label: String, painter: Painter){
+    var value by remember{mutableStateOf("")}
+    Column() {
+                Text(heading, fontSize = 20.sp, color = Color.Black,
+                    fontFamily = roboto, fontWeight = FontWeight.Medium)
+                Spacer(modifier = Modifier.height(8.dp))
+                Box(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(40.dp))
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .background(color = Color.White, shape = RoundedCornerShape(20.dp))
+                ) {
+                    TextField(
+                        value = value,
+                        onValueChange = { it ->
+                            value = it
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(5.dp),
+                        placeholder = {
+                            Text(
+                            label,
+                            color = Color.Black,
+                            fontFamily = roboto,
+                        )
+                        },
+                        leadingIcon = {
+                            Image(
+                                painter = painter,
+                                contentDescription = "nerd_icon",
+                                modifier = Modifier.size(35.dp)
+                            )
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            cursorColor = Color.Black,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedIndicatorColor = Color.White
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            KeyboardCapitalization.None,
+                            autoCorrect = true,
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next
+                        ),
+                    )
+                }
+    }
+}
--- app/src/main/java/com/example/passwordmanager/View/NewPassword/NewPasswordScreen/NewPasswordScreen.kt
+++ app/src/main/java/com/example/passwordmanager/View/NewPassword/NewPasswordScreen/NewPasswordScreen.kt
@@ -1,34 +1,55 @@
 package com.example.passwordmanager.View.NewPassword.NewPasswordScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.MaterialTheme.colors
 import com.example.passwordmanager.R
+import com.example.passwordmanager.View.NewPassword.Componenets.InputCard
 import com.example.passwordmanager.View.NewPassword.Componenets.NewPasswordScreenHeading
 import com.example.passwordmanager.ui.theme.roboto
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewPasswordScreen(){
+fun NewPasswordScreen() {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,6 +58,24 @@ fun NewPasswordScreen(){
     ) {
         Spacer(modifier = Modifier.height(30.dp))
         NewPasswordScreenHeading()
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(
+            modifier = Modifier
+                .height(500.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(20.dp),
+            colors = CardDefaults.cardColors(Color.Transparent)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = colorResource(R.color.theme4), RoundedCornerShape(20.dp))
+                    .padding(10.dp)
+            ) {
+                InputCard("Enter Username", "Username", painterResource(id = R.drawable.nerd_face))
+
+            }
+        }
     }
 }
 
