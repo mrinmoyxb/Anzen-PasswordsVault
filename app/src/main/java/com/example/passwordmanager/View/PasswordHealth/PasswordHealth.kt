c25ff829b2db66155d6b9799c7475b183b13becb
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-13 10:47:12 +0530

    completed the password checker algorithm

--- app/src/main/java/com/example/passwordmanager/View/PasswordHealth/PasswordHealth.kt
+++ app/src/main/java/com/example/passwordmanager/View/PasswordHealth/PasswordHealth.kt
@@ -1,16 +1,16 @@
 package com.example.passwordmanager.View.PasswordHealth
 
+import androidx.lifecycle.ViewModel
 
 
-fun main() {
-    val password: String = "12345ab"
-    val regex = Regex("[!@#$%^&*()_+{}\":;'<>?,./]")
+class PasswordChecker : ViewModel() {
+    var password: String = ""
 
-    var upperCase: Boolean = false
-    var upperCount: Int = 0
+    var uppercase: Boolean = false
+    var uppercount: Int = 0
 
-    var lowerCase: Boolean = false
-    var lowerCount: Int = 0
+    var lowercase: Boolean = false
+    var lowercount: Int = 0
 
     var numbers: Boolean = false
     var numberCount: Int = 0
@@ -18,66 +18,61 @@ fun main() {
     var symbols: Boolean = false
     var symbolsCount: Int = 0
 
+    val regex = Regex("[!@#$%^&*()_+{}\":;'<>?,./]")
+    var length: Int = 0
+    var complexityScore: Int = 0
 
-    val length: Int = password.length
-    println("length: $length")
+    // 1. Length of password
+    fun lengthPassowrd(){
+        length = password.length
+    }
 
+    // 2.function to check type and number of characters in password
+    fun checkCharacters(){
+        for(char in password){
+            if(char.isUpperCase()){
+                uppercase=true
+                uppercount++
+            }
+            else if(char.isLowerCase()){
+                lowercase=true
+                lowercount++
+            }
+            else if(char.isDigit()){
+                numbers=true
+                numberCount++
+            }
+            else if(regex.containsMatchIn(char.toString())){
+                symbols=true
+                symbolsCount++
+            }
 
-
-    for(char in password){
-        if(char.isUpperCase()){
-            upperCase = true
-            upperCount++
         }
-        else if(char.isLowerCase()){
-            lowerCase = true
-            lowerCount++
+    }
+
+    // 3. Calculate complexity score:
+    fun calculateComplexityScore(){
+        if(uppercount>0){
+            complexityScore++
         }
-        else if(char.isDigit()){
-            numbers = true
+        if(lowercount>0){
+            complexityScore++
+        }
+        if(numberCount>0){
             numberCount++
         }
-        else if(regex.containsMatchIn(char.toString())){
-            symbols = true
+        if(symbolsCount>0){
             symbolsCount++
         }
     }
 
-    println("Uppercase: $upperCase")
-    println("Lowercase: $lowerCase")
-    println("Numbers: $numbers")
-    println("symbols: $symbols")
+    // 4. CalculateButton
+    fun calculateButton(){
+        lengthPassowrd()
+        checkCharacters()
+        calculateComplexityScore()
+    }
 
-    println()
-    println("Password Details: ")
-    println("password: $password")
-    println("length: $length")
-    println("upper: $upperCount")
-    println("lower: $lowerCount")
-    println("numbers: $numberCount")
-    println("symbols: $symbolsCount")
 
-    println()
-    calPass(10, 20, 0, 2)
-}
 
-fun calPass(u: Int, l: Int, n: Int, s: Int){
-    var complexity = 0
-    if(u>0){
-        complexity++
-        println("y1")
-    }
-    if(l>0){
-        complexity++
-        println("y2")
-    }
-    if(n>0){
-        complexity++
-        println("y3")
-    }
-    if(s>0){
-        complexity++
-        println("y4")
-    }
-    println("Complexity score: $complexity")
 }
