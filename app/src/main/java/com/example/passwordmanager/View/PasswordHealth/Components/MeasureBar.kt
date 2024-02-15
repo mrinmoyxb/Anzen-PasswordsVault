4c3a80c9e9e3f6b6c5b2555b2d270e9776b253e4
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-15 21:59:38 +0530

    background 1

---
+++ app/src/main/java/com/example/passwordmanager/View/PasswordHealth/Components/MeasureBar.kt
@@ -0,0 +1,70 @@
+package com.example.passwordmanager.View.PasswordHealth.Components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ContentAlpha
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun CustomComponent(
+    canvasSize: Dp = 300.dp,
+    indicatorValue: Int = 0,
+    maxIndicator: Int = 100,
+    backgroundIndicatorColor: Color = Color.Blue,
+    backgroundIndicatorStrokeWidth: Float = 100f
+){
+    Column(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = backgroundIndicatorColor,
+                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+                )
+            }
+    ){
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayCustomComposable(){
+    CustomComponent()
+}
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+){
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width-componentSize.width)/2f,
+            y = (size.height-componentSize.width)/2f
+        )
+    )
+}
